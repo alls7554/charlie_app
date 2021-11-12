65180c8196f149d3f243c340e56a259db281a21f
alls7554 <alls970459@gmail.com>
2021-11-12 12:51:52 +0900

    remove ununsed image

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Navigation from './src/components/Navigation';
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
 
